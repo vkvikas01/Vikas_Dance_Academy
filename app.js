cc1e481dc3afad47cfafd7fdc3abfbcbb47c97b0
Vikas <Vikassharma26543@gmail.com>
2021-08-31 00:05:33 +0530

    change package.jason

---
+++ app.js
@@ -0,0 +1,52 @@
+const express=require("express");
+const path=require("path");
+const app=express();
+const bodyparser=require("body-parser")
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/contactDance', {useNewUrlParser: true, useUnifiedTopology: true});
+const port=800;
+
+
+//define mongoose schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    Phone: String,
+    Gender: String,
+    address: String,
+    email: String
+    
+  });
+
+  const contact = mongoose.model('contact', contactSchema);
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+ 
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    const params = {}
+    res.status(200).render('home.pug', params);
+})
+app.get('/contact', (req, res)=>{
+    const params = {}
+    res.status(200).render('contact.pug', params);
+})
+app.post("/contact", (req, res)=>{
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+    res.send("This item has been saved to the database")
+    }).catch(()=>{
+    res.status(400).send("item was not saved to the databse")
+})
+// res.status(200).render('contact.pug');
+})
+
+
+
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
